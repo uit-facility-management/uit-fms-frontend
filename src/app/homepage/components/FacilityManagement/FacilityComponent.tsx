31950c06e03056f443f8ef2310bc02fd6d2d495f
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-15 14:58:31 +0700

    feat: sua

--- src/app/homepage/components/FacilityManagement/FacilityComponent.tsx
+++ src/app/homepage/components/FacilityManagement/FacilityComponent.tsx
@@ -1,3 +1,3 @@
 export default function ToolsComponent() {
-  return <div className="text-lg">Component: Quản lý dụng cụ</div>;
+  return <div className="text-lg">Component: Quản lý cơ sở vật chất</div>;
 }
