122266b7a0a4857b08fbf3080e7ae4b0f4e7f5b0
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-16 13:09:07 +0700

    fix: fix login dto

---
+++ src/feature/auth/auth.api.ts
@@ -0,0 +1,16 @@
+import { appApi } from "@/lib/appApi";
+import type { LoginRequest, LoginResponse } from "./type";
+
+export const authApi = appApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation<LoginResponse, LoginRequest>({
+      query: (body) => ({
+        url: "/auth/login",
+        method: "POST",
+        body,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
